9ff748e315012e30bbabc873647e5ec718bd6cde
Martin Ghiena <josemartinghiena@gmail.com>
2020-07-07 22:11:23 -0300

    Se inicia proyecto Ajedrez

---
+++ piezas.py
@@ -0,0 +1,9 @@
+class Pieza:
+    def __init__(self, nombre, equipo):
+        """ Constructor de piezas. Superclase """
+        self.nombre
+        self.equipo
+
+class Rey(Piezza):
+    def __init__(self):
+        super().__init__(self)
