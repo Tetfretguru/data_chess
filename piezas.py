36c22328f3023c25e66d1c707d1c99d7353dae9b
Martin Ghiena <josemartinghiena@gmail.com>
2020-07-07 23:10:37 -0300

    Se hizo primera instancia de super clase

--- piezas.py
+++ piezas.py
@@ -3,10 +3,15 @@ class Pieza:
         """ Constructor de piezas. Superclase """
         self.nombre = nombre 
         self.equipo = equipo
+    
+    
 
 class Rey(Pieza):
     def __init__(self, nombre, equipo):
         super().__init__(nombre, equipo)
+    
+    def __repr__(self):
+        return self.nombre + ' ' + self.equipo
     
     def mover(self):
         """ El rey se mueve en todas (8) direcciones de a un paso """
