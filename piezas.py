0b9a24ad2cfa001657fe9e0d32e801259428ed70
Martin Ghiena <josemartinghiena@gmail.com>
2020-07-09 11:43:01 -0300

    Se agilizan las creaciones de instancias de Piezas mediante loops

--- piezas.py
+++ piezas.py
@@ -1,4 +1,4 @@
-from bokeh.plotting import show, output_file, figure
+#from bokeh.plotting import show, output_file, figure
 import random
 
 class Pieza:
@@ -24,12 +24,73 @@ class Rey(Pieza):
         """ Come en todas las direcciones """
         pass
 
-if __name__ == '__main__':
-    nombre = 'King'
-    equipo = 'N'
-    pieza = Rey(nombre, equipo)
+class Reina(Pieza):
+    def __init__(self, nombre, equipo):
+        super().__init__(nombre, equipo)
+    
+    def __repr__(self):
+        return self.nombre + ' ' + self.equipo
+    
+    def mover(self):
+        """ El rey se mueve en todas (8) direcciones de a un paso """
+        pass
 
-    print(pieza)
+    def comer(self):
+        """ Come en todas las direcciones """
+        pass
+
+
+
+if __name__ == '__main__':
+    
+    reyes = []
+    reinas = []
+ 
+
+    #Cantidad de reyes posibles 
+    r = 0
+    while r <= len(reyes):
+        nombre = 'King'
+        equipo = 'W'
+        pieza = Rey(nombre, equipo)
+        reyes.append(pieza)
+        r += 1
+        if r == 1:
+            nombre = 'King'
+            equipo = 'B'
+            pieza = Rey(nombre, equipo)
+            reyes.append(pieza)
+            break
+    
+    for rey in reyes:
+        print(f'Pieza: {rey}')
+    print('---' * 6)
+
+    q = 0
+    while q <= len(reinas):
+        nombre = 'Queen'
+        equipo = 'W'
+        pieza = Reina(nombre, equipo)
+        reinas.append(pieza)
+        q += 1
+        if q == 1:
+            nombre = 'Queen'
+            equipo = 'B'
+            pieza = Reina(nombre, equipo)
+            reinas.append(pieza)
+            break
+    
+    for reina in reinas:
+        print(f'Pieza: {reina}')
+    
+
+
+    
+   
+   
+  
+
+    
 
     
 
