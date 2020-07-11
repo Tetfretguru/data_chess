a83a7a589de4c7c83c603321968595105b40a73f
Martin Ghiena <josemartinghiena@gmail.com>
2020-07-10 21:43:30 -0300

    Cree un nuevo modulo para modelar un tablero. Los proximos commits se harán en la rama homonima al modulo

---
+++ tablero.py
@@ -0,0 +1,7 @@
+import piezas as pzs 
+
+""" En este modulo pienso generar un algoritmo para poder
+    dividir el tablero en triangulos para asi ordenar en grupos
+    y detectar en qué lugar del mismo se encuentra cada una de las piezas 
+    dependiendo sus coeficientes
+"""
